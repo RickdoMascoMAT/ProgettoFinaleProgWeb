1d5f987860347fbe056d806ae55d923fe2789c60
RickdoMasco <riccardo.mascotto2003@gmail.com>
2026-01-10 16:13:13 +0100

    add TypeScript types and update Tailwind CSS configuration

---
+++ src/types/index.tsx
@@ -0,0 +1,13 @@
+export type UUID = string;
+export type DisplayName = string;
+export type Rank = string;
+
+export type Timestamp = number;
+
+export type Optional<T> = T | null;
+
+export type LoadingState<T> = {
+    isLoading: boolean;
+    data?: T;
+    error?: string;
+};
